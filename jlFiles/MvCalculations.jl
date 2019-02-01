1a2ee8fa2fa2a18ab983cf737606fa9110634660
Soderlind <Paul.Soderlind@unisg.ch>
2019-02-01 17:55:49 +0100

    polishing

--- jlFiles/MvCalculations.jl
+++ jlFiles/MvCalculations.jl
@@ -5,15 +5,14 @@ Calculate the std and portfolio weights of a portfolio (with a given mean, musta
 
 """
 function MVCalc(mustar,μ,Σ) #the std of a portfolio on MVF of risky assets
-    n    = length(μ)
-    oneV = ones(n)
-    Σ_1  = inv(Σ)
-    A    = μ'Σ_1*μ
-    B    = μ'Σ_1*oneV
-    C    = oneV'Σ_1*oneV
-    λ    = (C*mustar - B)/(A*C-B^2)
-    δ    = (A-B*mustar)/(A*C-B^2)
-    w    = Σ_1 *(μ*λ.+δ)
+    n     = length(μ)
+    Σ_1   = inv(Σ)
+    A     = μ'Σ_1*μ
+    B     = μ'Σ_1*ones(n)
+    C     = ones(n)'Σ_1*ones(n)
+    λ     = (C*mustar - B)/(A*C-B^2)
+    δ     = (A-B*mustar)/(A*C-B^2)
+    w     = Σ_1 *(μ*λ.+δ)
     StdRp = sqrt(w'Σ*w)
     return StdRp,w                    #std and portfolio weights
 end
@@ -38,12 +37,11 @@ end
 Calculate the tangency portfolio
 """
 function MVTangencyP(μ,Σ,Rf)           #calculates the tangency portfolio
-    n     = length(μ)
-    oneV  = ones(n)
-    μe    = μ .- Rf                    #expected excess returns
-    Σ_1   = inv(Σ)
-    w     = Σ_1 *μe/(oneV'Σ_1*μe)
-    muT   = w'μ + (1-sum(w))*Rf
+    n    = length(μ)
+    μe   = μ .- Rf                    #expected excess returns
+    Σ_1  = inv(Σ)
+    w    = Σ_1 *μe/(ones(n)'Σ_1*μe)
+    muT  = w'μ + (1-sum(w))*Rf
     StdT = sqrt(w'Σ*w)
     return w,muT,StdT                  #portolio weights, mean and std
 end
