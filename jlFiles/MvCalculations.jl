5156f833910950b932ffe753242386be39b3bb15
Soederlind <Paul.Soederlind@gmail.com>
2022-07-19 09:36:30 +0200

    use OSQP, some editing

--- jlFiles/MvCalculations.jl
+++ jlFiles/MvCalculations.jl
@@ -3,19 +3,14 @@
 
 Calculate the std and weights of a portfolio (with mean return μstar) on MVF of risky assets.
 
-# Remark
-- Only (λ,δ) and thus (w,stdRp) depend on μstar. We could therefore speed up the computations a bit
-by doing the loop over different μstar values inside the function (and thus not recalculate Σ_1,a,b,c).
 """
-function MVCalc(μstar,μ,Σ)
+function MVCalc(μstar,μ,Σ) #the std of a portfolio on MVF of risky assets
     n    = length(μ)
-    Σ_1  = inv(Σ)
-    a    = μ'Σ_1*μ
-    b    = μ'Σ_1*ones(n)
-    c    = ones(n)'Σ_1*ones(n)
-    λ    = (c*μstar - b)/(a*c-b^2)
-    δ    = (a-b*μstar)/(a*c-b^2)
-    w    = Σ_1 *(μ*λ.+δ)
+    A     = [Σ        μ ones(n);   #A is just a name of this matrix, it's not asset A
+             μ'       0 0;
+             ones(n)' 0 0];
+    wλδ   = A\[zeros(n);μstar;1]
+    w     = wλδ[1:n]
     StdRp = sqrt(w'Σ*w)
     return StdRp,w
 end
@@ -27,11 +22,11 @@ end
 Calculate the std and portfolio weights of a portfolio (with a given mean, μstar) on MVF of (risky assets,riskfree)
 """
 function MVCalcRf(μstar,μ,Σ,Rf)
-    μe    = μ .- Rf
+    μᵉ    = μ .- Rf                 #expected excess returns
     Σ_1   = inv(Σ)
-    w     = (μstar-Rf)/(μe'Σ_1*μe) * Σ_1*μe
+    w     = (μstar-Rf)/(μᵉ'Σ_1*μᵉ) * Σ_1*μᵉ
     StdRp = sqrt(w'Σ*w)
-    return StdRp,w                    #std and portfolio weights
+    return StdRp,w
 end
 
 
@@ -41,11 +36,11 @@ end
 Calculate the tangency portfolio
 """
 function MVTangencyP(μ,Σ,Rf)           #calculates the tangency portfolio
-    n    = length(μ)
-    μe   = μ .- Rf                    #expected excess returns
-    Σ_1  = inv(Σ)
-    w    = Σ_1 *μe/(ones(n)'Σ_1*μe)
-    muT  = w'μ + (1-sum(w))*Rf
-    StdT = sqrt(w'Σ*w)
-    return w,muT,StdT                  #portolio weights, mean and std
+    n     = length(μ)
+    μᵉ    = μ .- Rf                    #expected excess returns
+    Σ_1   = inv(Σ)
+    w     = Σ_1 *μᵉ/(ones(n)'Σ_1*μᵉ)
+    muT   = w'μ + (1-sum(w))*Rf
+    StdT  = sqrt(w'Σ*w)
+    return w,muT,StdT
 end
