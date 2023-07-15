d349b6b512f95e55d635852dbdf3dd0a03dca2ae
Paul Soderlind <Paul.Soederlind@gmail.com>
2023-07-15 10:47:20 +0200

    update before 2023 course

--- jlFiles/MvCalculations.jl
+++ jlFiles/MvCalculations.jl
@@ -1,15 +1,15 @@
 """
-    MVCalc(μstar,μ,Σ)
+    MVCalc(μˣ,μ,Σ)
 
-Calculate the std and weights of a portfolio (with mean return μstar) on MVF of risky assets.
+Calculate the std and weights of a portfolio (with mean return μˣ) on MVF of risky assets.
 
 """
-function MVCalc(μstar,μ,Σ) #the std of a portfolio on MVF of risky assets
+function MVCalc(μˣ,μ,Σ) #the std of a portfolio on MVF of risky assets
     n    = length(μ)
     A     = [Σ        μ ones(n);   #A is just a name of this matrix, it's not asset A
              μ'       0 0;
              ones(n)' 0 0];
-    wλδ   = A\[zeros(n);μstar;1]
+    wλδ   = A\[zeros(n);μˣ;1]
     w     = wλδ[1:n]
     StdRp = sqrt(w'Σ*w)
     return StdRp,w
@@ -17,14 +17,14 @@ end
 
 
 """
-    MVCalcRf(μstar,μ,Σ,Rf)
+    MVCalcRf(μˣ,μ,Σ,Rf)
 
-Calculate the std and portfolio weights of a portfolio (with a given mean, μstar) on MVF of (risky assets,riskfree)
+Calculate the std and portfolio weights of a portfolio (with a given mean, μˣ) on MVF of (risky assets,riskfree)
 """
-function MVCalcRf(μstar,μ,Σ,Rf)
+function MVCalcRf(μˣ,μ,Σ,Rf)
     μᵉ    = μ .- Rf                 #expected excess returns
     Σ_1   = inv(Σ)
-    w     = (μstar-Rf)/(μᵉ'Σ_1*μᵉ) * Σ_1*μᵉ
+    w     = (μˣ-Rf)/(μᵉ'Σ_1*μᵉ) * Σ_1*μᵉ
     StdRp = sqrt(w'Σ*w)
     return StdRp,w
 end
